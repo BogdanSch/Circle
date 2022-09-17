805dc36fc18328940bc9f6b293fc99d3b2fcf7e7
Bohdan <bogmira777@gmail.com>
2022-09-17 21:49:02 +0200

    Added S for circle

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 $d = rand(10, 500) * 2;
+const PI = 3.14
+
+$s = PI * ((d/2)*(d/2))
 
 $size = $d."px";
-echo "Diameter: ".$d;
+echo "S = ".$d;
 
 echo "<div style=\"width: $size; height: $size; background: blue; border-radius: 50%;\"></div>";
